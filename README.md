ef81db1262a2533a886b04e0b7b29d941463f2fb
Mark Ide <git@cranstonide.com>
2017-06-01 19:54:40 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# twitter-timeline-to-png
---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p output/
+
+# Open a fake X server session and run cutycapt to snapshot the Twitter account.
+xvfb-run --server-args="-screen 0, 1280x1200x24" cutycapt --url=https://mobile.twitter.com/cranstonide --out=twitter-timeline.png
+
+# Format the image.
+convert twitter-timeline.png -flatten -trim +repage twitter-timeline.png
+convert twitter-timeline.png -gravity North -crop 100%x20 twitter-timeline.png
+
+# Move it into output, will be deployed into S3.
+mv twitter-timeline.png output/twitter-timeline.png
